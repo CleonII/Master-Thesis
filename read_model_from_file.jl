430af46cfee1c15dabbc0e3475825fb20106c291
CleonII <60782665+CleonII@users.noreply.github.com>
2021-11-17 13:56:29 +0100

    Update read_model_from_file.jl

--- read_model_from_file.jl
+++ read_model_from_file.jl
@@ -1,14 +1,14 @@
-using PyCall
 using BenchmarkTools
 using OrdinaryDiffEq
-using Plots
-include("test.jl")
+#using Plots
+include("./test.jl")
 
-du = Vector{Float64}(undef, 3)
-u0 = [1.0, 1.0, 1.0]
-p = 1.0
+u0 = initialSpeciesValue
+du = Vector{Float64}(undef, length(u0))
+p = trueParameterValues
 tspan = [0.0, 10.0]
 prob = ODEProblem(ODE_model!, u0, tspan, p)
-sol = @btime OrdinaryDiffEq.solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8)
-plt1 = plot(sol)
-display(plt1)
+sol = @btime OrdinaryDiffEq.solve(prob, TRBDF2(), reltol=1e-18, abstol=1e-18, dtmax = 0.01)
+print("done")
+#plt1 = plot(sol)
+#display(plt1)
