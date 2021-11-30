df5bb8b4cb4dd1df36f3e3a145f848908c76e79e
CleonII <60782665+CleonII@users.noreply.github.com>
2021-11-30 10:45:45 +0100

    Update read_modellingToolkit_from_file.jl

--- read_modellingToolkit_from_file.jl
+++ read_modellingToolkit_from_file.jl
@@ -1,6 +1,7 @@
 using ModelingToolkit, OrdinaryDiffEq
 
-include("./testMTK.jl")
+modelName = "model_Beer_MolBioSystems2014"
+include("./RewrittenModels/" * modelName * ".jl")
 
 sys = ode_order_lowering(sys)
 
@@ -13,3 +14,8 @@ c = trueConstantsValues
 tspan = (0.0,10.0)
 prob = ODEProblem(sys,u0,tspan,[p;c],jac=true)
 sol = solve(prob,TRBDF2())
+using Plots
+plotly()
+#plt1 = plot(sol,vars=(Cells))
+plt2 = plot(sol)
+display(plt2)
