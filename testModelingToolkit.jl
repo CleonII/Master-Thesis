b0c7e15fa3fe9d4101d7db53aef3e1b8e8aa46fa
CleonII <60782665+CleonII@users.noreply.github.com>
2021-11-30 10:46:20 +0100

    Update testModelingToolkit.jl

--- testModelingToolkit.jl
+++ testModelingToolkit.jl
@@ -1,27 +1,20 @@
 
 using ModelingToolkit, OrdinaryDiffEq
 
-@parameters t σ ρ β
-@variables x(t) y(t) z(t) c(t) d(t)
+@parameters σ ρ β tau
+@variables t x(t) y(t) z(t) c(t) dummy(t)
 D = Differential(t)
 
 continuous_events = [ # This time we have a vector of pairs
-    [t ~ 10] => [c ~ 10, d ~ 10]; [t ~ 11, t ~ 12] => [d ~ 3]
+    [t ~ tau] => [c ~ 1]
 ]
 # write rules either as events or as functions 
 
-
-fun_z(t, a, b) = t < 10 ? a : b
-fun1(d) = begin; if d > 3; 1; else; 2; end; end
-@register fun_z(t,a,b)
-@register fun1(d)
-
-
 eqs = [D(D(x)) ~ σ*(y-x),
        D(y) ~ x*(ρ-z)-y,
-       D(z) ~ x*y - β*z + c*d * fun1(d),
+       D(z) ~ x*y - β*z + c,
        D(c) ~ 0,
-       D(d) ~ 0]
+       D(dummy) ~ tau]
 
 @named sys = ODESystem(eqs, t, continuous_events = continuous_events)
 sys = ode_order_lowering(sys)
@@ -31,16 +24,17 @@ u0 = [D(x) => 2.0,
       y => 0.0,
       z => 0.0,
       c => 0.0,
-      d => 5.0]
+      dummy => 0.0]
 
 p  = [σ => 28.0,
       ρ => 10.0,
-      β => 8/3]
+      β => 8/3,
+      tau => 10.0]
 
 
 tspan = (0.0,100.0)
 prob = ODEProblem(sys,u0,tspan,p,jac=true)
-sol = @time solve(prob,Tsit5())
+sol = solve(prob,Tsit5())
 using Plots; plot(sol,vars=(x,y))
 tUse = (sol.t .> 8) .== (sol.t .< 12)
 plot(sol.t[tUse], sol[4,tUse])
