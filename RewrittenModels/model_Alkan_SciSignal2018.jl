74301a45d3660cfda1aecb05a9cbd2ebdd78dbc6
CleonII <60782665+CleonII@users.noreply.github.com>
2021-12-03 15:59:03 +0100

    Update model_Alkan_SciSignal2018.jl

--- RewrittenModels/model_Alkan_SciSignal2018.jl
+++ RewrittenModels/model_Alkan_SciSignal2018.jl
@@ -1,8 +1,13 @@
+# Model name: model_Alkan_SciSignal2018
 # Number of parameters: 52
 # Number of species: 36
 
 ### Define independent and dependent variables
 @variables t Cells_Dead(t) Cells_Apo1(t) pATR_G2(t) yH2AX_G2(t) Wip1_mRNA_S(t) pATM_S(t) p21_G2(t) Cells_SSBDamage_S(t) pp53_S(t) pATM_G2(t) Cells_Apo2(t) Cells_Cycle_G2(t) Cells_Apo_ReOx(t) yH2AX_S(t) p21_mRNA_S(t) Wip1_S(t) Wip1_mRNA_G2(t) Cells_Apo4(t) Cells_DSBDamage_G2(t) Cells_Apo(t) pChk2_G2(t) Wip1_G2(t) pATR_S(t) p21_S(t) Cells_DSBDamage_S(t) p21_mRNA_G2(t) Space(t) pChk2_S(t) Cells_Cycle_S(t) Cells_Apo3(t) pChk1_G2(t) pDNAPK_S(t) pChk1_S(t) pDNAPK_G2(t) pp53_G2(t) Cells(t)
+
+### Define variable parameters
+
+### Define dummy variable
 
 ### Define parameters
 @parameters k_dox_apo p_p53_act_atm p_chk1_dea_wip1 k_rep_hr p_p53_act_chk1 k_rep_nhej k_death k_dox_kd init_Cells_Cycle_G2_rel k_apo_dsb_g2 k_rep_s p_p21_turn p_h2ax_act_atm p_p53_act_chk2 p_atr_act_ssb p_mrna_exp_inh_dox_kd p_chk2_dea_wip1 k_cyc_arr_chk2 p_wip1_mrna_turn p_mrna_exp_inh_dox Gem_level k_ssb_to_dsb k_rep_nhej_sat Dox_level k_lyse k_damage_sn38_ssb p_chk1_act p_atr_act_atm k_apo_ssb k_apo_dsb_s k_cyc_arr_chk1 p_h2ax_act_atr k_death_delay p_h2ax_act_dnapk k_damage_dox_dsb p_chk2_act p_atm_act_dsb kt p_dnapk_act k_damage_gem_ssb p_p53_act_atr init_Cells_Cycle_S_rel k_death_reox kt_apo k_ssb_to_dsb_sn38 init_Space init_Cells p_dnapk_dea_wip1 SN38_level p_h2ax_dea p_p21_mrna_turn p_wip1_turn
@@ -13,9 +18,9 @@
 ### Define an operator for the differentiation w.r.t. time
 D = Differential(t)
 
-### Events ###
-
 ### Function definitions ###
+
+### Events ###
 
 ### Derivatives ###
 eqs = [
@@ -26,7 +31,7 @@ D(yH2AX_G2) ~ +1.0 * (-pATM_G2 * p_h2ax_act_atm * (yH2AX_G2 - 1))+1.0 * (-pATR_G
 D(Wip1_mRNA_S) ~ +1.0 * (p_wip1_mrna_turn * pp53_S / (p_mrna_exp_inh_dox * ((Dox_level)^(6) / ((Dox_level)^(6) + (p_mrna_exp_inh_dox_kd)^(6))) + 1))-1.0 * (Wip1_mRNA_S * p_wip1_mrna_turn),
 D(pATM_S) ~ +1.0 * (-(Cells_DSBDamage_S * p_atm_act_dsb * (pATM_S - 1)) / (Cells + Cells_Apo + Cells_Apo1 + Cells_Apo2 + Cells_Apo3 + Cells_Apo4 + Cells_Cycle_G2 + Cells_Cycle_S + Cells_Apo_ReOx + Cells_DSBDamage_G2 + Cells_DSBDamage_S + Cells_SSBDamage_S))-1.0 * (pATM_S / 24),
 D(p21_G2) ~ +1.0 * (p21_mRNA_G2 * p_p21_turn)-1.0 * (p21_G2 * p_p21_turn),
-D(Cells_SSBDamage_S) ~ -1.0 * (Cells_SSBDamage_S * k_dox_apo * ((Dox_level)^(6) / ((Dox_level)^(6) + (k_dox_kd)^(6))))+1.0 * (Gem_level * Cells_Cycle_S * k_damage_gem_ssb)+1.0 * (SN38_level * Cells_Cycle_S * k_damage_sn38_ssb)-1.0 * (Cells_SSBDamage_S * (k_ssb_to_dsb + k_ssb_to_dsb_sn38 * (SN38_level > 0) * 1 + (SN38_level < 0) * 0))-1.0 * (Cells_SSBDamage_S * k_rep_s * pATR_S)-1.0 * (Cells_SSBDamage_S * k_apo_ssb * pp53_S)-1.0 * (Cells_SSBDamage_S * kt_apo / (k_cyc_arr_chk1 * pChk1_S + 1)),
+D(Cells_SSBDamage_S) ~ -1.0 * (Cells_SSBDamage_S * k_dox_apo * ((Dox_level)^(6) / ((Dox_level)^(6) + (k_dox_kd)^(6))))+1.0 * (Gem_level * Cells_Cycle_S * k_damage_gem_ssb)+1.0 * (SN38_level * Cells_Cycle_S * k_damage_sn38_ssb)-1.0 * (Cells_SSBDamage_S * (k_ssb_to_dsb + k_ssb_to_dsb_sn38 * ((SN38_level > 0) * 1 + (SN38_level <= 0) * 0)))-1.0 * (Cells_SSBDamage_S * k_rep_s * pATR_S)-1.0 * (Cells_SSBDamage_S * k_apo_ssb * pp53_S)-1.0 * (Cells_SSBDamage_S * kt_apo / (k_cyc_arr_chk1 * pChk1_S + 1)),
 D(pp53_S) ~ +1.0 * (-pATR_S * p_p53_act_atr * (pp53_S - 1))+1.0 * (-pChk1_S * p_p53_act_chk1 * (pp53_S - 1))+1.0 * (-pATM_S * p_p53_act_atm * (pp53_S - 1))+1.0 * (-pChk2_S * p_p53_act_chk2 * (pp53_S - 1))-1.0 * (pp53_S / 24),
 D(pATM_G2) ~ +1.0 * (-(Cells_DSBDamage_G2 * p_atm_act_dsb * (pATM_G2 - 1)) / (Cells + Cells_Apo + Cells_Apo1 + Cells_Apo2 + Cells_Apo3 + Cells_Apo4 + 2 * Cells_Cycle_G2 + Cells_Apo_ReOx + Cells_DSBDamage_G2 + Cells_DSBDamage_S + Cells_SSBDamage_S))-1.0 * (pATM_G2 / 24),
 D(Cells_Apo2) ~ +1.0 * (Cells_Apo1 * k_death_delay)-1.0 * (Cells_Apo2 * k_death_delay)-1.0 * (Cells_Apo2 * k_dox_apo * ((Dox_level)^(6) / ((Dox_level)^(6) + (k_dox_kd)^(6)))),
@@ -43,7 +48,7 @@ D(pChk2_G2) ~ +1.0 * (-pATM_G2 * p_chk2_act * (pChk2_G2 - 1))-1.0 * (pChk2_G2 / 
 D(Wip1_G2) ~ +1.0 * (Wip1_mRNA_G2 * p_wip1_turn)-1.0 * (Wip1_G2 * p_wip1_turn),
 D(pATR_S) ~ +1.0 * (-(Cells_SSBDamage_S * p_atr_act_ssb * (pATR_S - 1)) / (Cells + Cells_Apo + Cells_Apo1 + Cells_Apo2 + Cells_Apo3 + Cells_Apo4 + Cells_Cycle_G2 + Cells_Cycle_S + Cells_Apo_ReOx + Cells_DSBDamage_G2 + Cells_DSBDamage_S + Cells_SSBDamage_S))+1.0 * (-pATM_S * p_atr_act_atm * (pATR_S - 1))-1.0 * (pATR_S / 24),
 D(p21_S) ~ +1.0 * (p21_mRNA_S * p_p21_turn)-1.0 * (p21_S * p_p21_turn),
-D(Cells_DSBDamage_S) ~ -1.0 * (Cells_DSBDamage_S * k_dox_apo * ((Dox_level)^(6) / ((Dox_level)^(6) + (k_dox_kd)^(6))))+1.0 * (Cells_SSBDamage_S * (k_ssb_to_dsb + k_ssb_to_dsb_sn38 * (SN38_level > 0) * 1 + (SN38_level < 0) * 0))-1.0 * (Cells_DSBDamage_S * k_rep_nhej * pDNAPK_S / (Cells_DSBDamage_S + k_rep_nhej_sat))-1.0 * (Cells_DSBDamage_S * k_apo_dsb_s * pp53_S)-1.0 * (Cells_DSBDamage_S * kt_apo / (k_cyc_arr_chk1 * pChk1_S + 1)),
+D(Cells_DSBDamage_S) ~ -1.0 * (Cells_DSBDamage_S * k_dox_apo * ((Dox_level)^(6) / ((Dox_level)^(6) + (k_dox_kd)^(6))))+1.0 * (Cells_SSBDamage_S * (k_ssb_to_dsb + k_ssb_to_dsb_sn38 * ((SN38_level > 0) * 1 + (SN38_level <= 0) * 0)))-1.0 * (Cells_DSBDamage_S * k_rep_nhej * pDNAPK_S / (Cells_DSBDamage_S + k_rep_nhej_sat))-1.0 * (Cells_DSBDamage_S * k_apo_dsb_s * pp53_S)-1.0 * (Cells_DSBDamage_S * kt_apo / (k_cyc_arr_chk1 * pChk1_S + 1)),
 D(p21_mRNA_G2) ~ +1.0 * (p_p21_mrna_turn * pp53_G2 / (p_mrna_exp_inh_dox * ((Dox_level)^(6) / ((Dox_level)^(6) + (p_mrna_exp_inh_dox_kd)^(6))) + 1))-1.0 * (p21_mRNA_G2 * p_p21_mrna_turn),
 D(Space) ~ -1.0 * (Cells * Space * kt / init_Space)+1.0 * (Cells_Dead * k_lyse),
 D(pChk2_S) ~ +1.0 * (-pATM_S * p_chk2_act * (pChk2_S - 1))-1.0 * (pChk2_S / 24)-1.0 * (Wip1_S * pChk2_S * p_chk2_dea_wip1),
