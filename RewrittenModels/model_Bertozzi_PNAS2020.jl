d1593f9b760e39414cd7d859802629d5c31c0c06
CleonII <60782665+CleonII@users.noreply.github.com>
2022-01-02 17:32:22 +0100

    Update model_Bertozzi_PNAS2020.jl

--- RewrittenModels/model_Bertozzi_PNAS2020.jl
+++ RewrittenModels/model_Bertozzi_PNAS2020.jl
@@ -20,30 +20,16 @@
 ### Define an operator for the differentiation w.r.t. time
 D = Differential(t)
 
-### Function definitions ###
-Io(Trigger_NY, Io_NY, Io_CA) = Trigger_NY * Io_NY + (1 - Trigger_NY) * Io_CA
-@register Io(Trigger_NY, Io_NY, Io_CA)
-Pop(Trigger_NY, Pop_NY, Pop_CA) = Trigger_NY * Pop_NY + (1 - Trigger_NY) * Pop_CA
-@register Pop(Trigger_NY, Pop_NY, Pop_CA)
-Ro_CA_ld(Ro_CA) = 0.5 * Ro_CA
-@register Ro_CA_ld(Ro_CA)
-Ro_NY_ld(Ro_NY) = 0.5 * Ro_NY
-@register Ro_NY_ld(Ro_NY)
-gamma(Trigger_NY, gamma_NY, gamma_CA) = Trigger_NY * gamma_NY + (1 - Trigger_NY) * gamma_CA
-@register gamma(Trigger_NY, gamma_NY, gamma_CA)
-Parameterised_time(t, gamma_CA) = t * gamma_CA
-@register Parameterised_time(t, gamma_CA)
-
 ### Events ###
 continuous_events = [
-[t ~ Trigger_NY * Lockdown_NY_start + (1 - Trigger_NY) * Lockdown_CA_start] => [Ro ~ Trigger_NY * Ro_NY_ld(Ro_NY) + (1 - Trigger_NY) * Ro_CA_ld(Ro_CA)], [t ~ Trigger_NY * Lockdown_NY_end + (1 - Trigger_NY) * Lockdown_CA_end] => [Ro ~ Trigger_NY * Ro_NY + (1 - Trigger_NY) * Ro_CA], [Trigger_Lockdown ~ 0.5] => [Ro ~ Trigger_NY * Ro_NY_ld(Ro_NY) + (1 - Trigger_NY) * Ro_CA_ld(Ro_CA)]
+[t ~ Trigger_NY * Lockdown_NY_start + (1 - Trigger_NY) * Lockdown_CA_start] => [Ro ~ Trigger_NY * (0.5 * Ro_NY) + (1 - Trigger_NY) * (0.5 * Ro_CA)], [t ~ Trigger_NY * Lockdown_NY_end + (1 - Trigger_NY) * Lockdown_CA_end] => [Ro ~ Trigger_NY * Ro_NY + (1 - Trigger_NY) * Ro_CA], [Trigger_Lockdown ~ 0.5] => [Ro ~ Trigger_NY * (0.5 * Ro_NY) + (1 - Trigger_NY) * (0.5 * Ro_CA)]
 ]
 
 ### Derivatives ###
 eqs = [
-D(Infected) ~ +1.0 * (USA___CA__NY * (gamma(Trigger_NY, gamma_NY, gamma_CA) * Ro * Infected * Susceptible))-1.0 * (USA___CA__NY * gamma(Trigger_NY, gamma_NY, gamma_CA) * Infected),
-D(Recovered) ~ +1.0 * (USA___CA__NY * gamma(Trigger_NY, gamma_NY, gamma_CA) * Infected),
-D(Susceptible) ~ -1.0 * (USA___CA__NY * (gamma(Trigger_NY, gamma_NY, gamma_CA) * Ro * Infected * Susceptible)),
+D(Infected) ~ +1.0 * (USA___CA__NY * ((Trigger_NY * gamma_NY + (1 - Trigger_NY) * gamma_CA) * Ro * Infected * Susceptible))-1.0 * (USA___CA__NY * (Trigger_NY * gamma_NY + (1 - Trigger_NY) * gamma_CA) * Infected),
+D(Recovered) ~ +1.0 * (USA___CA__NY * (Trigger_NY * gamma_NY + (1 - Trigger_NY) * gamma_CA) * Infected),
+D(Susceptible) ~ -1.0 * (USA___CA__NY * ((Trigger_NY * gamma_NY + (1 - Trigger_NY) * gamma_CA) * Ro * Infected * Susceptible)),
 D(Ro) ~ 0,
 D(dummyVariable) ~ +Ro_NY+Lockdown_NY_end+Lockdown_CA_end+Trigger_NY+Ro_CA+Lockdown_CA_start+Lockdown_NY_start+Trigger_Lockdown
 ]
@@ -52,9 +38,9 @@ D(dummyVariable) ~ +Ro_NY+Lockdown_NY_end+Lockdown_CA_end+Trigger_NY+Ro_CA+Lockd
 
 ### Initial species concentrations ###
 initialSpeciesValues = [
-Infected => Io(Trigger_NY, Io_NY, Io_CA) / Pop(Trigger_NY, Pop_NY, Pop_CA),
+Infected => (Trigger_NY * Io_NY + (1 - Trigger_NY) * Io_CA) / (Trigger_NY * Pop_NY + (1 - Trigger_NY) * Pop_CA),
 Recovered => 0.0,
-Susceptible => (Pop(Trigger_NY, Pop_NY, Pop_CA) - Infected) / Pop(Trigger_NY, Pop_NY, Pop_CA),
+Susceptible => ((Trigger_NY * Pop_NY + (1 - Trigger_NY) * Pop_CA) - Infected) / (Trigger_NY * Pop_NY + (1 - Trigger_NY) * Pop_CA),
 Ro => 2.7,
 dummyVariable => 0.0]
 
