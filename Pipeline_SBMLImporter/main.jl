2c28643639cf6e3e89f3d05559328ac5de992e66
Viktor <vikhas@student.chalmers.se>
2022-04-18 23:00:44 +0200

    Fixed paths to work with both linux and windows

--- Pipeline_SBMLImporter/main.jl
+++ Pipeline_SBMLImporter/main.jl
@@ -1,6 +1,6 @@
 using PyCall
 
-include(pwd() * "/Pipeline_SBMLImporter/write_modellingToolkit_to_file.jl")
+include(joinpath(pwd(), "Pipeline_SBMLImporter", "write_modellingToolkit_to_file.jl"))
 
 function getSBMLFiles(path)
     files = readdir(path)
@@ -17,14 +17,14 @@ end
 function main()
     libsbml = pyimport("libsbml")
     reader = libsbml.SBMLReader()
-    readPath = pwd() * "/Pipeline_SBMLImporter/SBML"
-    writePath = pwd() * "/Pipeline_SBMLImporter/JuliaModels"
+    readPath = joinpath(pwd(), "Pipeline_SBMLImporter", "SBML")
+    writePath = joinpath(pwd(), "Pipeline_SBMLImporter", "JuliaModels")
     fixDirectories(writePath)
     
 
     files = getSBMLFiles(readPath)
     for file in files
-        document = reader[:readSBML](pwd() * "/Pipeline_SBMLImporter/SBML/" * file)
+        document = reader[:readSBML](joinpath(pwd(), "Pipeline_SBMLImporter", "SBML", file))
         model = document[:getModel]() # Get the model
         writeODEModelToFile(libsbml, model, file[1:end-4], writePath)
     end
