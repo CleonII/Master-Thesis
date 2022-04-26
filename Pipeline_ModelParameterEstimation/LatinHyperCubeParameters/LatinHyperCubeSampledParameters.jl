90e75e26f42b49a10a1f6b8d83aa238bc7d9335b
Viktor <vikhas@student.chalmers.se>
2022-04-26 11:02:15 +0200

    Function for creating evenly spread initial guesses

---
+++ Pipeline_ModelParameterEstimation/LatinHyperCubeParameters/LatinHyperCubeSampledParameters.jl
@@ -0,0 +1,26 @@
+using LatinHypercubeSampling, CSV, DataFrames
+
+function createSamples(nDims)
+    dirName = joinpath(pwd(), "Pipeline_ModelParameterEstimation", "LatinHyperCubeParameters", )
+    fileName = string(nDims) * ".csv"
+    plan = LHCoptim(50, nDims, 1000)[1]
+    CSV.write(joinpath(dirName, fileName), DataFrame(plan, :auto))
+end
+
+function getSamples(nDims, lowerBounds, upperBounds, index)
+    dirName = joinpath(pwd(), "Pipeline_ModelParameterEstimation", "LatinHyperCubeParameters")
+    fileName = string(nDims) * ".csv"
+    if fileName in readdir(dirName)
+        plan = Matrix(CSV.read(joinpath(dirName, fileName), DataFrame))
+    else
+        plan = LHCoptim(50, nDims, 1000)[1]
+        CSV.write(joinpath(dirName, fileName), DataFrame(plan, :auto))
+    end
+    bounds = Array{Tuple{Float64, Float64}}(undef, nDims)
+    for i in eachindex(bounds)
+        bounds[i] = (lowerBounds[i], upperBounds[i])
+    end
+
+    scaled_plan = vec(Matrix(scaleLHC(plan, bounds))[index, :])
+    return scaled_plan
+end
