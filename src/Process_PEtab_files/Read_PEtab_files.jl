813505e348136d4c82dea08e86d52114480fa643
Damiano Ognissanti <ognissanti@hotmail.se>
2023-02-03 16:02:28 +0100

    Added check for datatype or value

--- src/Process_PEtab_files/Read_PEtab_files.jl
+++ src/Process_PEtab_files/Read_PEtab_files.jl
@@ -50,8 +50,9 @@ function readPEtabFiles(pathYAML::String)
     experimentalConditions = CSV.read(pathConditions, DataFrame, stringtype=String)
     measurementsData = CSV.read(pathMeasurements, DataFrame, stringtype=String)
     parametersData = CSV.read(pathParameters, DataFrame, stringtype=String)
-    observablesData = CSV.read(pathObservables, DataFrame, stringtype=String)
-    
+    observablesData = CSV.read(pathObservables, DataFrame, stringtype=String)    
+    checkFilesForCorrectDataType(experimentalConditions, measurementsData, parametersData, observablesData)
+
     return experimentalConditions, measurementsData, parametersData, observablesData
 end
 function readPEtabFiles(petabModel::PEtabModel)
@@ -60,6 +61,126 @@ function readPEtabFiles(petabModel::PEtabModel)
     measurementsData = CSV.read(petabModel.pathMeasurements, DataFrame, stringtype=String)
     parametersData = CSV.read(petabModel.pathParameters, DataFrame, stringtype=String)
     observablesData = CSV.read(petabModel.pathObservables, DataFrame, stringtype=String)
-    
+    checkFilesForCorrectDataType(experimentalConditions, measurementsData, parametersData, observablesData)
+
     return experimentalConditions, measurementsData, parametersData, observablesData
+end
+
+
+"""
+checkForPeTabFile(fileSearchFor::String, dirModel::String)::String
+
+Helper function to check in dirModel if a file starting with fileSearchFor exists. 
+If true return file path.
+"""
+function checkForPeTabFile(fileSearchFor::String, dirModel::String)::String
+
+filesDirModel = readdir(dirModel)
+iUse = findall(x -> occursin(fileSearchFor, x), filesDirModel)
+if length(iUse) > 1 
+    printf("Error : More than 1 file starting with %s in %s\n", fileSearchFor, filesDirModel)
+end
+if length(iUse) == 0
+    printf("Error : No file starting with %s in %s\n", fileSearchFor, filesDirModel)
+end
+
+return dirModel * filesDirModel[iUse[1]]
+end
+
+
+
+"""
+checkDataFrameColumns(dataFrame, dataFrameName, colsToCheck, allowedTypesVec, requiredCols)
+        Goes through each column from colsToCheck in dataFrame and checks 
+        if each column is of any of the DataTypes specified in allowedTypesVec[colIndex].
+        Returns true if all columns are ok and false otherwise.
+        requiredCols is an array of mandatory columns. If a mandatory column is missing 
+        an error is thrown, and if a mandatory column contains missing rows a warning is
+        thrown.
+"""
+function checkDataFrameColumns(dataFrame, dataFrameName, colsToCheck, allowedTypesVec, requiredCols)
+
+    check = true
+
+    for colIndex in eachindex(colsToCheck)
+
+        colName = colsToCheck[colIndex]
+        allowedTypes = allowedTypesVec[colIndex]
+
+        # If column is required and not present an error is thrown. 
+        if (colName in requiredCols) && !(colName in names(dataFrame))
+            throw(PEtabFileError("Required column '" * colName * "' missing in file '" * dataFrameName * "'"))
+        # If column is required and there are missing values in that column a warning is thrown.
+        elseif (colName in requiredCols) && (Missing <: eltype(dataFrame[!, colName]))
+            println("Warning : Required column " * colName * " contains rows with missing values.")
+        # If column is not required and present the check is skipped.
+        elseif !(colName in requiredCols) && !(colName in names(dataFrame))
+            continue
+        end
+
+        # Extract column excluding missing values
+        colToCheck = dropmissing(dataFrame, colName)[!, colName]
+        dType = eltype(colToCheck)
+
+        if (allowedTypes isa Array{DataType, 1}) 
+            check &= dType <: Union{allowedTypes...}
+        elseif (allowedTypes isa Array{String, 1}) || (allowedTypes isa Array{Int64, 1})
+            check &= all(x->x in allowedTypes, colToCheck)
+        end
+
+        if !check
+            throw(PEtabFileError("Wrong DataType or value in file '" * dataFrameName * "' column '" * colName * "'. Must be: " * "[" * join(allowedTypes, ", ") * "]" * "."))
+        end    
+    
+    end
+
+end
+
+function checkFilesForCorrectDataType(experimentalConditions, measurementsData, parametersData, observablesData)
+
+    # Allowed DataTypes for the different columns in the files
+    stringTypes = [String]
+    numberTypes = [Float64, Int64]
+    stringOrNumberTypes = [String, Float64, Int64]
+    transformationTypes = ["lin", "log", "log10"]
+    distributionTypes = ["laplace", "normal"]
+    estimateTypes = [0,1]
+    priorParameterTypes = ["uniform", "normal", "laplace", "logNormal", "logLaplace", "parameterScaleUniform", "parameterScaleNormal", "parameterScaleLaplace"]
+    
+    # Check experimentalConditions
+    colsToCheck = ["conditionId", "conditionName"]
+    allowedTypesVec = [stringTypes, stringTypes]
+    requiredCols = ["conditionId"]
+    checkDataFrameColumns(experimentalConditions, "experimentalConditions", colsToCheck, allowedTypesVec, requiredCols)
+    # Check parameter columns in experimentalConditions
+    if "conditionName" in names(experimentalConditions)
+        colsToCheck = names(experimentalConditions[!,Not([:conditionId,:conditionName])])
+    else
+        colsToCheck = names(experimentalConditions[!,Not([:conditionId])])
+    end
+
+    if !isempty(colsToCheck)
+        allowedTypesVec = repeat([stringOrNumberTypes], length(colsToCheck))
+        checkDataFrameColumns(experimentalConditions, "experimentalConditions", colsToCheck, allowedTypesVec, [])
+    end
+
+    # Check measurementsData
+    colsToCheck =     ["observableId", "simulationConditionId", "measurement", "time", "preequilibrationConditionId", "observableParameters", "noiseParameters", "datasetId", "replicateId"]
+    allowedTypesVec = [stringTypes, stringTypes, numberTypes, numberTypes, stringTypes, stringOrNumberTypes, stringOrNumberTypes, stringOrNumberTypes, stringOrNumberTypes]
+    requiredCols = ["observableId", "simulationConditionId", "measurement", "time"]
+    checkDataFrameColumns(measurementsData, "measurementsData", colsToCheck, allowedTypesVec, requiredCols)
+
+    # Check parametersData
+    colsToCheck = ["parameterId", "parameterScale", "lowerBound", "upperBound", "nominalValue", "estimate", "parameterName", "initializationPriorType", "initializationPriorParameters", "objectivePriorType", "objectivePriorParameters"]
+    # Some models have missing parameters in their bounds even though it's mandatory, so we add Missing as an allowed DataType for these columns.
+    allowedTypesVec = [stringTypes, transformationTypes, numberTypes, numberTypes, numberTypes, estimateTypes, stringTypes, priorParameterTypes, stringTypes, priorParameterTypes, stringTypes]
+    requiredCols = ["parameterId", "parameterScale", "lowerBound", "upperBound", "nominalValue", "estimate"]
+    checkDataFrameColumns(parametersData, "parametersData", colsToCheck, allowedTypesVec, requiredCols)
+
+    # Check observablesData
+    colsToCheck = ["observableId", "observableFormula", "noiseFormula", "observableName", "observableTransformation", "noiseDistribution"]
+    allowedTypesVec = [stringTypes, stringTypes, stringOrNumberTypes, stringTypes, transformationTypes, distributionTypes]
+    requiredCols = ["observableId", "observableFormula", "noiseFormula"]
+    checkDataFrameColumns(observablesData, "observablesData", colsToCheck, allowedTypesVec, requiredCols)
+    
 end
